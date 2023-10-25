4e6327bb4ef8c35f7ee00d140e264951f1bbacb5
dv-15 <143259664+dv-15@users.noreply.github.com>
2023-10-26 00:34:40 +0530

    added second urls.py

--- students/urls.py
+++ students/urls.py
@@ -1,13 +1,24 @@
-from django.urls import path
-from.import views
+"""
+URL configuration for college_application project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
 
 urlpatterns = [
-    path("",views.students_detail,name="students_detail"),
-    path("insert",views.student_insert,name="student_insert"),
-    path("save",views.insert_save,name="insert_save"),
-    path("edit/<student_id>",views.student_edit,name="student_edit"),
-    path("save/<student_id>",views.student_updated,name="student_updated"),
-    path("remove/<student_id>",views.student_delete,name='student_delete'),
-    path("<student_id>",views.student_all_detail,name="student_all_detail"),
-    
+    path('admin/', admin.site.urls),
+    path("course/",include("course.urls")),
+    path("students/",include("students.urls")),
 ]
